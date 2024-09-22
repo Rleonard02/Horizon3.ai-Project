0069d4014b31b2f76406098fc70b8adb5571d8a1
michaelli-radianhub <michael.li@radianhub.com>
2024-09-22 15:57:12 -0400

    hi

---
+++ Frontend/my-app/src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h1>Login</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
