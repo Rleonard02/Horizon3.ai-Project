44881c19991bef54fdb5474feb0bfd760e5acb1a
michaelli-radianhub <michael.li@radianhub.com>
2024-09-22 16:47:52 -0400

    made express server

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import Login from "./Pages/Login";
+import Login from "./Pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
 
-        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ Frontend/my-app/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <div className="home-container">
+        <h1>Input</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
